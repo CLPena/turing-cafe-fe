91f6920c319f518d42e0fdb7b55458e783c7ec7b
Cristina Pena <cristinalpena@gmail.com>
2020-04-02 12:03:23 -0600

    Try to change date data to clean up display

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -3,6 +3,11 @@ import './ReservationCard.css';
 
 
 const ReservationCard = (props) => {
+  // let uglyDate = props.date;
+  // let dateArray = uglyDate.split('-')
+  // let prettyDate = `${dateArray[1]}/${dateArray[2]}`
+  //I started trying to clean up the date data but ran out of time!
+
   return (
     <div className='resy-card'>
       <p>NAME: {props.name}</p>
